a0f966e78be60953b52e0026e175e28bbbe33b32
fengluoxiao <fengluoxiao@yeah.net>
2026-04-25 23:25:29 +0800

    Initial Commit

---
+++ funPlayer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  funPlayer
+//
+//  Created by 枫洛萧 on 2026/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
